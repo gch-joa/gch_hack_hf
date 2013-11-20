8682fd290e4ef5be6aab9b677fcac12c34b1cf5a
Andry Joos <andry.joos@geocom.ch>
2013-11-20 10:31:43 +0100

    Daten

---
+++ project/hackathon/Loader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hackathon.data;
+
+namespace hackathon
+{
+    public class Loader
+    {
+        private string _filenameAbstimmungen;
+
+        public Loader(string abstimmungen)
+        {
+            _filenameAbstimmungen = abstimmungen;
+        }
+
+        public IEnumerable<Jahr> Load()
+        {
+            IList<Kanton> kantone = new List<Kanton>
+                {
+                    new Kanton("Bern"),
+                    new Kanton("Zürich"),
+                    new Kanton("Basel Stadt"),
+                    new Kanton("Basel Land"),
+                    new Kanton("Genf"),
+                    new Kanton("Waadt"),
+                    new Kanton("Wallis"),
+                    new Kanton("Fribourg"),
+                    new Kanton("Neuchatel"),
+                    new Kanton("Jura"),
+                    new Kanton("Aarau"),
+                    new Kanton("Solothurn"),
+                    new Kanton("Thurgau"),
+                    new Kanton("Schaffhausen"),
+                    new Kanton("St. Gallen"),
+                    new Kanton("Uri"),
+                    new Kanton("Schwyz"),
+                    new Kanton("Luzern")
+
+                };
+        }
+
+        
+    }
+}
