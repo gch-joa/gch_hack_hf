7b93a2f9ef66825b061b56b5e4b9bd75e2e2336d
cspoerri <chris@sporri.org>
2013-11-20 14:35:06 +0100

    fixed loop problem

--- project/hackathon/Views/AbstimmungsStatsView.xaml.cs
+++ project/hackathon/Views/AbstimmungsStatsView.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using hackathon.ViewModel;
+
 namespace hackathon.Views
 {
 	/// <summary>
@@ -23,7 +25,7 @@ namespace hackathon.Views
 		public AbstimmungsStatsView()
 		{
 			InitializeComponent();
-			DataContext = new AbstimmungsStatsView();
+			DataContext = new AbstimmungsViewModel();
 		}
 	}
 }
